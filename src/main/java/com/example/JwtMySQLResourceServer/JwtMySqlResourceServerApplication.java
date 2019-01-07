15c707478cb863d378fa9eab31e93253e0fd378e
Hugo Picollet <hugo.picollet@gmail.com>
2019-01-07 09:12:02 +0100

    Initial commit

---
+++ src/main/java/com/example/JwtMySQLResourceServer/JwtMySqlResourceServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.JwtMySQLResourceServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtMySqlResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtMySqlResourceServerApplication.class, args);
+	}
+
+}
+
